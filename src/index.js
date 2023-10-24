ce1b35be4bb5cda4cceb1310bc30ac0c49d5adab
Oliver Muellerklein <oli-ai-phd@protonmail.com>
2023-10-24 11:07:45 -0700

    HOTFIX: attempted fix to URL redirection error

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 /*const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,7 +15,9 @@ root.render(
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename = "/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
